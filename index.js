948c92827f36b6f519a6e20af7ca835e57d2fbb9
Ruchita Parmar <ruchitaparmar079@gmail.com>
2026-02-19 23:35:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const con=mysql.createConnection({
 	"user":process.env.DB_USER,
 	"password":process.env.DB_PASSWORD,
 	"database":process.env.DB_NAME,	
-	"port":3306
+	"port":3306,
+	  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 con.connect((err) => {
@@ -41,4 +44,5 @@ app.get("/find",(req,res)=>{
 const PORT=process.env.PORT || 9000;
 app.listen(PORT,()=>{
 	console.log("Ready to serve @",PORT);
+
 });
